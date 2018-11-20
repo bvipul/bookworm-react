ce5ec0b5cc641d5a11c3c4f73ee65dda157d061b
Vipul Basapati <basapativipulkumar@gmail.com>
2018-11-20 16:11:50 +0530

    Login Form with state and validate the form with Validator

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,23 +1,75 @@
 import React from "react";
-import { Form } from "semantic-ui-react";
+import { Form, Button } from "semantic-ui-react";
+import Validator from "validator";
+import InlineError from "./../messages/InlineError";
 
 class LoginForm extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			data: {
+				email: "",
+				password: ""
+			},
+			loading: false,
+			errors: {}
+		};
+	}
+
+	onChange = e => {
+		this.setState({
+			data: { ...this.state.data, [e.target.name]: e.target.value }
+		});
+	};
+
+	onSubmit = () => {
+		const errors = this.validate(this.state.data);
+
+		this.setState({ errors });
+
+		if (Object.keys(errors).length === 0) {
+			this.props.submit(this.state.data);
+		}
+	};
+
+	validate = ({ email, password }) => {
+		const errors = {};
+		if (!Validator.isEmail(email)) errors.email = "Invalid email";
+		if (!password) errors.password = "Can't be blank";
+		return errors;
+	};
+
 	render() {
+		const { errors } = this.state;
 		return (
-			<Form>
-				<Form.Field>
+			<Form onSubmit={this.onSubmit}>
+				<Form.Field error={errors.email ? true : false}>
 					<label htmlFor="email">Email:</label>
 					<input
 						name="email"
 						type="email"
 						id="email"
 						placeholder="abc@xyz.com"
+						onChange={this.onChange}
 					/>
+					{errors.email ? <InlineError message={errors.email} /> : ""}
 				</Form.Field>
-				<Form.Field>
+				<Form.Field error={errors.password ? true : false}>
 					<label htmlFor="password">Password:</label>
-					<input name="password" type="password" id="password" />
+					<input
+						name="password"
+						type="password"
+						id="password"
+						onChange={this.onChange}
+					/>
+					{errors.password ? (
+						<InlineError message={errors.password} />
+					) : (
+						""
+					)}
 				</Form.Field>
+				<Button primary type="submit" content="Login" />
 			</Form>
 		);
 	}
---
+++ src/components/messages/InlineError.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InlineError = ({ message }) => (
+	<span style={{ color: "#dd4b39" }}>{message}</span>
+);
+
+export default InlineError;
